b4ff6915299ca64c1b11645e5f5fb186fe32fa33
Raftfg <raftak99@gmail.com>
2023-08-22 08:08:28 +0100

    ok

---
+++ src/app/user-pages/forgetpassword/forgetpassword.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-forgetpassword",
+  templateUrl: "./forgetpassword.component.html",
+  styleUrls: ["./forgetpassword.component.scss"],
+})
+export class ForgetPasswordComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/user-pages/user-pages.module.ts
+++ src/app/user-pages/user-pages.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { ForgetPasswordComponent } from "./forgetpassword/forgetpassword.component";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-]
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "forgetpassword", component: ForgetPasswordComponent },
+];
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
-  ]
+  declarations: [LoginComponent, RegisterComponent, ForgetPasswordComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class UserPagesModule { }
+export class UserPagesModule {}
