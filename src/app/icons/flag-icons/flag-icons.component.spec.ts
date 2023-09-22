65b20b7a0564f8d17dcdad83eb20a3dc758a0868
sidoine-projects <sidoinea96@gmail.com>
2023-09-22 23:29:09 +0100

    ok

--- src/app/icons/flag-icons/flag-icons.component.spec.ts
+++ src/app/icons/flag-icons/flag-icons.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FlagIconsComponent } from './flag-icons.component';
 
@@ -6,7 +6,7 @@ describe('FlagIconsComponent', () => {
   let component: FlagIconsComponent;
   let fixture: ComponentFixture<FlagIconsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FlagIconsComponent ]
     })
--- src/app/pages/basic-elements/form/basic-elements.component.spec.ts
+++ src/app/pages/basic-elements/form/basic-elements.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormulaireComponent } from './basic-elements.component';
 
@@ -6,7 +6,7 @@ describe('FormulaireComponent', () => {
   let component: FormulaireComponent;
   let fixture: ComponentFixture<FormulaireComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FormulaireComponent ]
     })
